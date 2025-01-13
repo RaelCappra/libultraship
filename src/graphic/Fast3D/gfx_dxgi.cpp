8fdaef5c6cb8576a38e22a5c48c3a2483b9a5aa0
lilacLunatic <8488221+lilacLunatic@users.noreply.github.com>
2025-01-13 20:20:30 -0300

    fix6

--- src/graphic/Fast3D/gfx_dxgi.cpp
+++ src/graphic/Fast3D/gfx_dxgi.cpp
@@ -558,6 +558,8 @@ void gfx_dxgi_init(const char* game_name, const char* gfx_api_name, bool start_i
 	dxgi.raw_input_device[0].dwFlags = RIDEV_INPUTSINK;
 	dxgi.raw_input_device[0].hwndTarget = dxgi.h_wnd;
     RegisterRawInputDevices(dxgi.raw_input_device, 1, sizeof(dxgi.raw_input_device[0]));
+    GetCursorPos(&dxgi.mouse_pos);
+    ScreenToClient(dxgi.h_wnd, &dxgi.mouse_pos);
 }
 
 static void gfx_dxgi_set_fullscreen_changed_callback(void (*on_fullscreen_changed)(bool is_now_fullscreen)) {
@@ -592,19 +594,21 @@ static void gfx_dxgi_set_cursor_visibility(bool visible) {
 
 static void gfx_dxgi_set_mouse_pos(int32_t x, int32_t y) {
     SetCursorPos(x, y);
+    dxgi.mouse_pos.x = x;
+    dxgi.mouse_pos.x = y;
 }
 
 static void gfx_dxgi_get_mouse_pos(int32_t* x, int32_t* y) {
     POINT p = dxgi.mouse_pos;
-    ScreenToClient(dxgi.h_wnd, &p);
+    //ScreenToClient(dxgi.h_wnd, &p);
     *x = p.x;
     *y = p.y;
 }
 
 static void gfx_dxgi_get_mouse_delta(int32_t* x, int32_t* y) {
     if (dxgi.is_mouse_captured && dxgi.in_focus) {
-        *x = dxgi.mouse_delta.x;
-        *y = dxgi.mouse_delta.y;
+        *x = dxgi.mouse_delta.x * dxgi.current_width;
+        *y = dxgi.mouse_delta.y;dxgi.current_height
     } else {
         *x = 0;
         *y = 0;
